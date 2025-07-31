eb2055dbd7a15a94c4c9d9211f44a8dae11bc42d
RiTiK <168961238+ritikkumar8z@users.noreply.github.com>
2025-08-01 04:49:50 +0530

    Update Basic_Concept.js

--- Basics/Basic_Concept.js
+++ Basics/Basic_Concept.js
@@ -9,7 +9,7 @@ console.log('null === undefined = ', null === undefined);
 console.log('"5" == 5 = ', "5" == 5); 
 console.log('"5" === 5 = ', "5" === 5);
 console.log('0 == false = ', 0 == false); 
-console.log('0 === false = ', 0 === false);
+console.log('0 === false = ', 0 === false); 
 console.log('"0" == false = ', "0" == false); 
 console.log('"0" === false = ', "0" === false); 
 console.log('1 + "2" - 3 = ', 1 + "2" - 3); 
