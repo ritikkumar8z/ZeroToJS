ff9db55cb5233695190403578a82fdf92d62ee9c
RiTiK <168961238+ritikkumar8z@users.noreply.github.com>
2025-08-11 19:54:50 +0530

    Update dom-select.js

--- DOM/dom-select.js
+++ DOM/dom-select.js
@@ -2,7 +2,7 @@ const createElementBtn = document.getElementById("create-element-btn");
 const elementContainer = document.getElementById("element-container");
 
 createElementBtn.addEventListener("click", function() {
-  const newElement = document.createElement("div");
+  const newElement = document.createElement("div"); 
   newElement.classList.add("new-element");
   newElement.textContent = "This is a new dynamically created element!";
 
